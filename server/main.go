2704502230531f026d6b3bceef876f0445a29f24
DaehyunKim <daniel8926@gmail.com>
2026-01-14 00:28:03 +0900

    feat: enhance environment variable loading by adding support for multiple potential .env file paths

--- server/main.go
+++ server/main.go
@@ -4,16 +4,36 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	episodeusecase "eventsure-server/application/episode"
 	httprouter "eventsure-server/interface/http"
 	"eventsure-server/interface/http/controller"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func main() {
+	// Load .env file
+	workDir, err := os.Getwd()
+	if err == nil {
+		// 가능한 .env 파일 경로들
+		possiblePaths := []string{
+			filepath.Join(workDir, ".env"),             // 현재 디렉토리
+			filepath.Join(workDir, "..", ".env"),       // 상위 디렉토리
+			filepath.Join(workDir, "..", "..", ".env"), // 상위 상위 디렉토리
+		}
+
+		for _, envPath := range possiblePaths {
+			if err := godotenv.Load(envPath); err == nil {
+				log.Printf("Loaded .env file from: %s\n", envPath)
+				break
+			}
+		}
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
